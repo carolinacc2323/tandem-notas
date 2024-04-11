bbf679a5135b7fc893a5b71f5eb7c7a4109a86af
naomitandem <carolinacc.tandem@gmail.com>
2024-04-11 13:34:59 +0200

    empiezo lógica

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 //Api de localStorage (el de la izquierda almacena el de la derecha)
-localStorage.setItem('tarea02','fiesta')
-let tarea = localStorage.getItem('tarea02') 
-console.log(tarea)
-console.log(localStorage.length)
+// localStorage.setItem('tarea02','fiesta')
+// let tarea = localStorage.getItem('tarea02') 
+// console.log(tarea)
+// console.log(localStorage.length)
 
-for (let i = 0; i < localStorage.length; i++) {
-    let key = localStorage.key(i) // Clave donde esta guardada la info del Est.
-    console.log(key)
-    tarea = localStorage.getItem(key) // Info del Est. en formato JSON
-    console.log(tarea)
-}
+// for (let i = 0; i < localStorage.length; i++) {
+//     let key = localStorage.key(i) // Clave donde esta guardada la info del Est.
+//     console.log(key)
+//     tarea = localStorage.getItem(key) // Info del Est. en formato JSON
+//     console.log(tarea)
+// }
 // localStorage.clear();
 
+// Pasos filezilla
---
+++ main2.js
@@ -0,0 +1,21 @@
+// Recoger la información 
+function getinfo(){
+    // Devuelve el texto
+    console.log('texto ingresado')
+    return 'text ingresado'
+}
+// Almacenarla en localStorage
+function setLocalInfo(){
+    // Guardar la info en localStorage
+    return true;
+}
+    function showInfo(){
+    // Leerla y mostrarla por pantalla
+    // Sino hay notas mostrar no hay nada
+        return true;
+    }
+    // Limpiarla
+
+    function cleanInfo(){
+        // Limpiar el localStorage y limpiar en la pantalla
+    }
